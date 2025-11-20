711e3456314c1f1c55f131685fca62317368dcd3
sunil.tailor <sunil.tailor@indexfeed.com>
2025-11-20 10:21:33 +0000

    feat: First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, DevContainer!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5020, debug=True)
